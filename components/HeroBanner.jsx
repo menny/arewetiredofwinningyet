0b573a02dae750cc3b2fd6bf6bcbeba0246d0b12
Menny Even Danan <menny@evendanan.net>
2017-03-29 22:36:19 -0400

    adding citation to quote

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,11 +1,12 @@
-var React = require('react');
+import React from 'react';
+import Link from 'link-react';
 
 var HeroBanner = React.createClass({
     render: function() {
         return (
             <p id="hero">
                 <div id="hero-quote">
-                    <h2>"Nobody knew health care could be so complicated"</h2>
+                    <h2>"Nobody knew health care could be so complicated"<Link className="citation" href="http://www.cnn.com/2017/02/27/politics/trump-health-care-complicated/">&nbsp;&nbsp;</Link></h2>
                     <p>- President Donald J. Trump</p>
                 </div>
             </p>
--- css/TopBanner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class TopBanner extends React.Component {
-  render () {
-    return (<p id="top-banner">ARE WE TIRED OF WINNING YET?</p>);
-  }
-}
-
-module.exports = TopBanner;
