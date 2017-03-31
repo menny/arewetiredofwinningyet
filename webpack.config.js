584232be18f52e1ee974b92eec9dd215fbe94afb
Menny Even Danan <menny@evendanan.net>
2017-03-31 10:39:52 -0400

    optimizations for mobile devices

--- webpack.config.js
+++ webpack.config.js
@@ -5,23 +5,25 @@ var BUILD_DIR = path.resolve(__dirname, 'public');
 var APP_DIR = path.resolve(__dirname, 'components');
 
 var config = {
-  entry: APP_DIR + '/main.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module : {
-    loaders : [
-      {
-        test: /\.(js|jsx)$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-            presets: ['es2015', 'react']
-        }
-      }
-    ]
-  }
+    entry: APP_DIR + '/main.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.(js|jsx)$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    },
+    devServer: {
+        contentBase: __dirname,
+        compress: true,
+        port: 9000
+    }
 };
-
-module.exports = config;
+module.exports = config
