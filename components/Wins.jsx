a206093568cb9f65c6c6680b0454a6ae3312eff9
Menny Even Danan <menny@evendanan.net>
2017-04-03 23:42:31 -0400

    adding Rationale to each entry

--- components/Wins.jsx
+++ components/Wins.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import Link from 'link-react';
+import ReactTooltip from 'react-tooltip';
 
 import winsData from './data/wins.json';
 
 function Win(props) {
-  return <div id="win-container" className={"card "+props.winState}>
-        <h3 id="win-title">{props.title}<span className={"win-state "+props.winState}>&nbsp;{props.score}&nbsp;</span></h3>
-        <p id="win-details">{props.details}<Link className="citation" href={props.link}>&nbsp;&nbsp;</Link></p>
-    </div>;
+    const tooltipId = "tooltip_"+Math.abs(Math.random());
+
+    return <div id="win-container" className={"card "+props.winState}>
+            <h3 id="win-title">{props.title}
+                <span data-tip="React-tooltip" data-for={tooltipId} className={"win-state "+props.winState}>
+                    &nbsp;{props.score}&nbsp;
+                </span>
+                <ReactTooltip id={tooltipId} place="left" multiline="true" effect="solid" class='rationale-tooltip'>
+                    <u>Why?</u><br/>
+                    { props.rationale }
+                </ReactTooltip>
+            </h3>
+            <p id="win-details">{props.details}<Link className="citation" href={props.link}>&nbsp;&nbsp;</Link></p>
+        </div>;
 }
 
 class Wins extends React.Component {
@@ -20,7 +31,7 @@ class Wins extends React.Component {
                         if (win.state == "fail") failsCount++;
                         if (win.state == "success") winsCount++;
                         const score = winsCount+":"+failsCount;
-                        winsElements.unshift(<Win key={"possible_win_"+index} title={win.title} winState={win.state} score={score} details={win.text} link={win.link}/>)
+                        winsElements.unshift(<Win key={"possible_win_"+index} title={win.title} winState={win.state} score={score} details={win.text} link={win.link} rationale={win.rationale}/>)
                     })
 
         return (
